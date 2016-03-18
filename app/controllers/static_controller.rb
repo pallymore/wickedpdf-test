1466d564b3060983a85888b4eacc223081fce3be
Yurui Zhang <eraclaymore@hotmail.com>
2016-03-18 16:43:08 -0400

    add download feature

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,4 +10,15 @@ class StaticController < ApplicationController
     end
   end
 
+  def download
+    file = Tempfile.new ['test', '.pdf']
+    file.write(render_to_string(
+       pdf: "test_pdf",
+       template: "static/index.html.erb"
+     ).force_encoding('utf-8'))
+    send_file file
+  ensure
+    file.close
+  end
+
 end
